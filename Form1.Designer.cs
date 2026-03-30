ac755dab9ec1693d46dc53e1a36b98a71d711adf
JunHyoung Her <h3r0310@gmail.com>
2026-03-30 11:30:37 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CatchButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnTarget = new Button();
+            SuspendLayout();
+            // 
+            // btnTarget
+            // 
+            btnTarget.BackColor = SystemColors.ButtonHighlight;
+            btnTarget.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnTarget.ForeColor = Color.Blue;
+            btnTarget.Location = new Point(285, 153);
+            btnTarget.Name = "btnTarget";
+            btnTarget.Size = new Size(206, 85);
+            btnTarget.TabIndex = 0;
+            btnTarget.Text = "나를 잡아봐";
+            btnTarget.UseVisualStyleBackColor = false;
+            btnTarget.Click += button1_Click;
+            btnTarget.MouseEnter += button1_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnTarget);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnTarget;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+namespace CatchButton
+{
+    public partial class Form1 : Form
+    {
+        private int score = 0;
+        private int missCount = 0; // 놓친 횟수
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "버튼 잡기 게임 시작!";
+        }
+
+        private void RestartGame()
+        {
+            score = 0;
+            missCount = 0;
+            btnTarget.Enabled = true;
+            btnTarget.Location = new Point(285, 153); // 초기 위치로 복원
+            this.Text = "게임 재시작! | 점수: 0 | 놓침: 0/10";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 제목과 함께
+            score = score + 100;
+            MessageBox.Show($"버튼 잡기에 성공하셨네요. 축하드립니다.~!\n현재 점수: {score}", "버튼 잡기 성공");
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            // 놓친 횟수 증가
+            missCount++;
+
+            // 10번 놓치면 게임 오버
+            if (missCount >= 10)
+            {
+                btnTarget.Enabled = false;
+                var result = MessageBox.Show(
+                    $"Game Over!\n최종 점수: {score}\n놓친 횟수: {missCount}\n\n다시 시작하시겠습니까?", 
+                    "게임 종료", 
+                    MessageBoxButtons.YesNo, 
+                    MessageBoxIcon.Stop);
+
+                this.Text = $"Game Over | 최종 점수: {score}";
+
+                if (result == DialogResult.Yes)
+                {
+                    RestartGame();
+                }
+
+                return; // 더 이상 실행하지 않음
+            }
+
+            // 1. 난수 생성기 준비
+            Random rd = new Random();
+
+            // 2. 가용 영역 계산 (버튼이 폼 테두리에 걸리지 않게 보호)
+            // ClientSize는 타이틀 바와 테두리를 제외한 실제 흰 도화지 영역임
+            int maxX = this.ClientSize.Width;
+            int maxY = this.ClientSize.Height;
+
+            // 3. 랜덤 좌표 추출 (0 ~ 최대 가용치 사이)
+            int nextX = rd.Next(0, maxX);
+            int nextY = rd.Next(0, maxY);
+
+            // 4. 위치 할당 (새로운 Point 객체 생성)
+            btnTarget.Location = new Point(nextX, nextY);
+
+            score = score - 5;
+
+            // 5. 시각적 피드백 (폼 제목 표시줄에 좌표 출력)
+            this.Text = $"버튼 위치: ({nextX}, {nextY}) | 점수: {score} | 놓침: {missCount}/10";
+
+            //System.Media.SystemSounds.Exclamation.Play();
+            Console.Beep();
+
+            // 버튼 크기를 점점 작게 (최소 크기 제한)
+            //if (btnTarget.Width > 50)
+            //{
+            //    btnTarget.Size = new Size((int)(btnTarget.Width * 0.9), (int)(btnTarget.Height * 0.9));
+            //}
+
+        }
+    }
+}
